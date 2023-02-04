0597ca332a50fbc358e10396b970068958f26681
Khanh Cao <caokhanh03.ck@gmail.com>
2023-02-04 15:15:10 +0700

    Update greedy.py

--- Code/Heuristic/greedy.py
+++ Code/Heuristic/greedy.py
@@ -2,13 +2,11 @@ import numpy as np
 import sys
 
 def input_():
-    [N,M] = [int(x) for x in sys.stdin.readline().split()]  # n: số môn thi, m số lượng phòng thi
-    d = [int(x) for x in sys.stdin.readline().split()]      # d: danh sách số lượng sinh viên đăng ký mỗi môn (d[i] là số lượng sinh viên đăng ký môn i)
-    c = [int(x) for x in sys.stdin.readline().split()]      # c: danh sách số lượng chỗ ngồi mỗi phòng (c[j] là số lượng chỗ ngồi của phòng j)
+    [N,M] = [int(x) for x in sys.stdin.readline().split()]                                      # n: số môn thi, m số lượng phòng thi
+    d = [int(x) for x in sys.stdin.readline().split()]                                          # d: danh sách số lượng sinh viên đăng ký mỗi môn (d[i] là số lượng sinh viên đăng ký môn i)
+    c = [int(x) for x in sys.stdin.readline().split()]                                          # c: danh sách số lượng chỗ ngồi mỗi phòng (c[j] là số lượng chỗ ngồi của phòng j)
     K = int(input())
-    conflicts = []                                          # conflicts: danh sách 2 môn thi (i,j) trùng sinh viên đăng ký 
-    for _ in range(K):
-        conflicts.append([int(x)-1 for x in sys.stdin.readline().split()])
+    conflicts = [list(map(lambda x: int(x)-1, sys.stdin.readline().split())) for _ in range(K)] # conflicts: danh sách 2 môn thi (i,j) trùng sinh viên đăng ký 
     return N,d,M,c,K,conflicts
 def schedule_exams(N, D, M, C, conflicts):
     schedule = [(0,0,0) for _ in range(N)]
@@ -16,27 +14,20 @@ def schedule_exams(N, D, M, C, conflicts):
     current_day = 0
     exclude = {}
     for i,j in conflicts:
-        if i not in exclude:
-            exclude[i] = [j]
-        else:
-            exclude[i].append(j)
-        if j not in exclude:
-            exclude[j] = [i]
-        else:
-            exclude[j].append(i)
-    
+        exclude[i] = exclude.get(i, []) + [j]
+        exclude[j] = exclude.get(j, []) + [i]
+
     # sắp xếp các môn thi theo số lượng đăng ký giảm dần
     exams = [(d, i) for i, d in enumerate(D)]
-    exams.sort(reverse=True)
-    
+    exams.sort(reverse=True, key=lambda x: x[0])
+
     # sắp xếp các phòng thi theo sức chứa giảm dần
     rooms = [(c, j) for j, c in enumerate(C)]
-    rooms.sort(reverse=True)
+    rooms.sort(reverse=True, key=lambda x: x[0])
 
     # gán môn thi vào kíp sớm nhất
     for exam in exams:
         d, i = exam
-        assigned = False
         for room in rooms:
             c, j = room
             for k in range(4):
@@ -44,20 +35,21 @@ def schedule_exams(N, D, M, C, conflicts):
                     # kiểm tra trường hợp có môn đã đăng ký hoặc phòng không đủ sức chứa
                     conflict = False
                     for ii in range(i):
-                        if (schedule[ii][0] == current_day and schedule[ii][2] == k) and ((ii in exclude[i]) or schedule[ii][1] == j ):
+                        if (schedule[ii][0] == current_day and schedule[ii][2] == k) and ((ii in exclude.get(i, [])) or schedule[ii][1] == j ):
                             conflict = True
                             break
                     if conflict:
                         continue
                     days[current_day][j][k] += d
+                    print("Mon %d xep vao phong %d kip %d"%(i+1,j+1,k+1))
                     schedule[i] = (current_day, j, k)
-                    assigned = True
                     break
-            if assigned:
-                break
-        if not assigned:
+            else:
+                continue
+            break
+        else:
             current_day += 1
-    
+            
     # trả về số ngày tối thiểu để diễn ra kì thi
     return current_day + 1, schedule
 
