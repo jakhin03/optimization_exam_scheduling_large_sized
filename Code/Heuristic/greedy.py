f73b9786ce4bcc844bf1c7ea15596bc02772d9ea
Khanh Cao <caokhanh03.ck@gmail.com>
2023-02-06 14:49:26 +0700

    Updated heuristic code and data input

--- Code/Heuristic/greedy.py
+++ Code/Heuristic/greedy.py
@@ -3,64 +3,77 @@
 import numpy as np
 import sys
 
-def input_():
-    [N,M] = [int(x) for x in sys.stdin.readline().split()]                                      # n: số môn thi, m số lượng phòng thi
-    d = [int(x) for x in sys.stdin.readline().split()]                                          # d: danh sách số lượng sinh viên đăng ký mỗi môn (d[i] là số lượng sinh viên đăng ký môn i)
-    c = [int(x) for x in sys.stdin.readline().split()]                                          # c: danh sách số lượng chỗ ngồi mỗi phòng (c[j] là số lượng chỗ ngồi của phòng j)
-    K = int(input())
-    conflicts = [list(map(lambda x: int(x)-1, sys.stdin.readline().split())) for _ in range(K)] # conflicts: danh sách 2 môn thi (i,j) trùng sinh viên đăng ký 
-    return N,d,M,c,K,conflicts
-    
+# def input_():
+#     [N,M] = [int(x) for x in sys.stdin.readline().split()]                                      # n: number of subjects, m number of rooms
+#     d = [int(x) for x in sys.stdin.readline().split()]                                          # d: list of number of students registered for each subject  (d[i] is number of students registerd subject i)
+#     c = [int(x) for x in sys.stdin.readline().split()]                                          # c: list of capacity of rooms (c[j] is the capacity of room j)
+#     K = int(input())
+#     conflicts = [list(map(lambda x: int(x)-1, sys.stdin.readline().split())) for _ in range(K)] # conflicts: list of 2 non-conflicting subjects (i,j)
+#     return N,d,M,c,K,conflicts
+
+def input_(filename):
+    with open(filename) as f:
+        input_list = f.readlines()
+        N = int(input_list[0])                                          # n: number of subjects, m number of rooms
+        d = [int(x) for x in input_list[1].strip().split()]             # n: number of subjects, m number of rooms
+        M = int(input_list[2])                                          # d: list of number of students registered for each subject  (d[i] is number of students registerd subject i)
+        c = [int(x) for x in input_list[3].strip().split()]             # c: list of capacity of rooms (c[j] is the capacity of room j)
+        K = int(input_list[4])                                          # k: number of conflicting pair
+        conflicts = [list(map(lambda x: int(x) - 1, ij.split())) for ij in input_list[5:]]    # conflicts: list of 2 conflicting subjects (i,j)
+        return N,d,M,c,K,conflicts
+
 def greedy(N, D, M, C, conflicts):
     time_table = [(0,0,0) for _ in range(N)]
     scheduled = np.zeros((N, M, 4), dtype=int)
-    current_day = 0
+    cur_day = 0
     exclude = {}
     for i,j in conflicts:
         exclude[i] = exclude.get(i, []) + [j]
         exclude[j] = exclude.get(j, []) + [i]
 
-    # sắp xếp các môn thi theo số lượng đăng ký giảm dần
+    # sort number of students registered for each subject in decreasing order
     exams = [(d, i) for i, d in enumerate(D)]
     exams.sort(reverse=True, key=lambda x: x[0])
+    print(len(exams))
 
-    # sắp xếp các phòng thi theo sức chứa giảm dần
+    # sort capacity of rooms in decreasing order
     rooms = [(c, j) for j, c in enumerate(C)]
     rooms.sort(reverse=True, key=lambda x: x[0])
 
-    # gán môn thi vào kíp sớm nhất
+    # assign subject to earliest available period
     for exam in exams:
         d, i = exam
         for room in rooms:
             c, j = room
             for k in range(4):
-                if (scheduled[current_day][j][k] + d <= c) and (scheduled[current_day][j][k] == 0):
-                    # kiểm tra trường hợp có môn đã đăng ký hoặc phòng không đủ sức chứa
+                if (scheduled[cur_day][j][k] + d <= c) and (scheduled[cur_day][j][k] == 0):
+                    # check if there is another subject registered or full of capacity of room
                     conflict = False
                     for ii in range(i):
-                        if (time_table[ii][0] == current_day and time_table[ii][2] == k) and ((ii in exclude.get(i, [])) or time_table[ii][1] == j ):
+                        if (time_table[ii][0] == cur_day and time_table[ii][2] == k) and ((ii in exclude.get(i, [])) or time_table[ii][1] == j ):
                             conflict = True
                             break
                     if conflict:
                         continue
-                    scheduled[current_day][j][k] += d
-                    time_table[i] = (current_day, j, k)
-                    print("Subject %d is time_tabled on day %d room %d and period %d"%(i+1, current_day+1, j+1, k+1))
+                    scheduled[cur_day][j][k] += d
+                    time_table[i] = (cur_day, j, k)
+                    # print(f"Subject {i+1} is scheduled at day {cur_day+1}, room {j+1}, period {k+1}") debug
                     break
             else:
                 continue
             break
         else:
-            current_day += 1
+            cur_day += 1
             
-    # trả về số ngày tối thiểu để diễn ra kì thi
-    return current_day + 1, time_table
+    # return the minimum days for the exam
+    return cur_day + 1, time_table
 
 def main():
-  n,d,m,c,k,conflicts = input_()
-  num_days, time_table = greedy(n,d,m,c, conflicts)
-  for exam, (i,j,k) in enumerate(time_table):
-    print(exam+1,k+1,j+1) #môn, kíp, phòng
-  
+    n,d,m,c,k,conflicts = input_(input())
+    num_days, time_table = greedy(n,d,m,c, conflicts)
+    for exam, (i,j,k) in enumerate(time_table):
+        print(f"Subject {exam+1} is assigned at day {i+1}, room {j+1}, and period {k+1}")
+    print(f"Minimum days for the exams: {num_days}")
+
 if __name__ == "__main__":
   main()
