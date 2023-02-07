8d875019ec6fe9466b511c04e7b4acfb133337b1
Khanh Cao <caokhanh03.ck@gmail.com>
2023-02-08 05:27:13 +0700

    Update greedy.py

--- Code/Heuristic/greedy.py
+++ Code/Heuristic/greedy.py
@@ -28,36 +28,36 @@ def greedy(N, D, M, C, conflicts):
     exams.sort(reverse=True, key=lambda x: x[0])
 
     # sort capacity of rooms in decreasing order
-     rooms.sort(reverse=True, key=lambda x: x[0])
+    rooms.sort(reverse=True, key=lambda x: x[0])
 
-# looping through each subject and assigning it to the earliest available shift in the first available room with enough capacity.
-scheduled_subs = 0
-while scheduled_subs < len(exams):
-    exam = exams[scheduled_subs]
-    d, i = exam
-    for room in rooms:
-        c, j = room
-        for k in range(4):
-            if (scheduled[cur_day][j][k] + d <= c) and (scheduled[cur_day][j][k] == 0):
-                # check if there is another subject registered or full of capacity of room
-                conflict = False
-                for ii in range(i):
-                    if (time_table[ii][0] == cur_day and time_table[ii][2] == k) and ((ii in exclude.get(i, [])) or time_table[ii][1] == j ): # check if there is another subject registered or conflict
-                        conflict = True
-                        break
-                if conflict:
-                    continue
-                scheduled[cur_day][j][k] += d
-                time_table[i] = (cur_day, j, k)
-                break
+    # looping through each subject and assigning it to the earliest available shift in the first available room with enough capacity.
+    scheduled_subs = 0
+    while scheduled_subs < len(exams):
+        exam = exams[scheduled_subs]
+        d, i = exam
+        for room in rooms:
+            c, j = room
+            for k in range(4):
+                if (scheduled[cur_day][j][k] + d <= c) and (scheduled[cur_day][j][k] == 0):
+                    # check if there is another subject registered or full of capacity of room
+                    conflict = False
+                    for ii in range(i):
+                        if (time_table[ii][0] == cur_day and time_table[ii][2] == k) and ((ii in exclude.get(i, [])) or time_table[ii][1] == j ): # check if there is another subject registered or conflict
+                            conflict = True
+                            break
+                    if conflict:
+                        continue
+                    scheduled[cur_day][j][k] += d
+                    time_table[i] = (cur_day, j, k)
+                    break
+            else:
+                continue
+            break
         else:
-            continue
-        break
-    else:
-        #If there is no room left in the last period, increasing the current day by 1
-        cur_day += 1
-        scheduled_subs -=1
-    scheduled_subs += 1
+            #If there is no room left in the last period, increasing the current day by 1
+            cur_day += 1
+            scheduled_subs -=1
+        scheduled_subs += 1
             
     # return the minimum days for the exam
     return cur_day + 1, time_table
